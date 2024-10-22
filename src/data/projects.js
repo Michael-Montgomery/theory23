279c23655052b85ed6f331052a66c6406f0fb9e0
michael-montgomery <thehealthyprogrammer@outlook.com>
2024-10-22 17:37:49 -0400

    added content to homepage,

---
+++ src/data/projects.js
@@ -0,0 +1,32 @@
+let projects = [
+    {
+        name: 'Esspresso Self',
+        imgUrl: 'https://images.pexels.com/photos/324028/pexels-photo-324028.jpeg?auto=compress&cs=tinysrgb&w=600',
+        about: 'ghdfjslhgjdfslg',
+        workAreas: ['Design', 'Development'],
+
+    },
+    {
+        name: 'Traditionals',
+        imgUrl: 'https://images.pexels.com/photos/1319460/pexels-photo-1319460.jpeg?auto=compress&cs=tinysrgb&w=600',
+        about: 'hfjdlahfjdlahjd',
+        workAreas: ['Design', 'Development'],
+        
+    },
+    {
+        name: 'The Abra',
+        imgUrl: 'https://images.pexels.com/photos/28993087/pexels-photo-28993087/free-photo-of-apostel-glasgow-skull-and-crossbones-sign.jpeg?auto=compress&cs=tinysrgb&w=600',
+        about: 'fdsfhdsjaklf',
+        workAreas: ['Design', 'Development'],
+        
+    },
+    {
+        name: "stasio's",
+        imgUrl: 'https://images.pexels.com/photos/28992220/pexels-photo-28992220/free-photo-of-assorted-antipasto-platter-with-cheese-and-meats.jpeg?auto=compress&cs=tinysrgb&w=600',
+        about: 'dfsdfsdfsdfds',
+        workAreas: ['Design', 'Development'],
+        
+    }
+];
+
+export default projects;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -21,7 +21,7 @@ export default function Home() {
 
                 <div id='home-content-wrapper' style={{ height: timerExpired ? 0 : '100vh' }}>
                     <p id='first-name'>Theory 23</p>
-                    <p id='tagline-text'>A Marketing Agengy</p>
+                    <p id='tagline-text'>A Design Agency</p>
 
                 </div>
             </div>
@@ -31,7 +31,17 @@ export default function Home() {
                     <Header></Header>
 
                     <div id='home-wrapper'>
-                        <div id='home-picture-wrapper'></div>
+                        <div id='home-picture-wrapper'></div><br></br>
+                        <div id='bio-wrapper'>
+                            <p className='logo'>Theory 23</p>
+                            <p>
+                                At Theory 23, we are a collective of creatives, engineers and support staff who specialize in creating unique and engaging 
+                                digital experiences. We create standout web applications using battle-tested methodologies to drive results we are proud to stand by. 
+                                While UI/UX design and engineering are our primary focus, our toolkit encompasses a wide array of skillsets. For more information, be sure to
+                                check out our services page. We are glad you're here and we can't wait to build something amazing together.
+                            </p>
+                        </div>
+                        <div id='team-wrapper'></div>
                     </div>
 
                 </div> : <></>
